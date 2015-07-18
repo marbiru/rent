fe9edb3c313b1b029414fc9926822eeadafb7502
marbiru <code@uribram.com>
2015-07-18 22:20:35 +0100

    feat: choices update a single array (up to p2)
    first working sections of rooms_price array, which simply takes all
    players' inputs and updates the values appropriately as play progresses

--- main_deprecated.js
+++ main_deprecated.js
@@ -10,14 +10,6 @@ var r1_name = "";
 var r2_name = "";
 var r3_name = "";
 
-// the main array for storing prices and room names. Updated often
-
-var rooms_prices = [
-    [r1_name,""],
-    [r2_name,""],
-    [r3_name,""],
-];
-
 // variables decided by player 1
 var p1_comp = ["", "", ""];
 
@@ -36,33 +28,23 @@ p3_name = "Player 3";
 
 r1_name = "The First Room";
 r2_name = "Second Room";
-r3_name = "The Third Room";
+r3_name = "Room 3";
 
 $( ".print_p1" ).append( p1_name );
 $( ".print_p2" ).append( p2_name );
 $( ".print_p3" ).append( p3_name );
 
-$( ".print_r1" ).append( r1_name );
-$( ".print_r2" ).append( r2_name );
-$( ".print_r3" ).append( r3_name );
+//END PLACEHOLDER
 
-rooms_prices = [
-    [r1_name,""],
-    [r2_name,""],
-    [r3_name,""],
-];
-
-//END PLACEHOLDER. WHEN DONE, REACTIVATE NEXT FN
-
-/* $(function() {
+$(function() {
     $( "#player_room_submit" ).click(function(){
-    	p1_name = $( "#p1_name" ).val();
+    	/* p1_name = $( "#p1_name" ).val();
         p2_name = $( "#p2_name" ).val();
         p3_name = $( "#p3_name" ).val();
         
         r1_name = $( "#r1_name" ).val();
         r2_name = $( "#r2_name" ).val();
-        r3_name = $( "#r3_name" ).val();
+        r3_name = $( "#r3_name" ).val(); */
 
         $( ".print_p1" ).append( p1_name );
         $( ".print_p2" ).append( p2_name );
@@ -72,38 +54,30 @@ rooms_prices = [
         $( ".print_r2" ).append( r2_name );
         $( ".print_r3" ).append( r3_name );
     });
-}); */
+});
 
-/* $(function() {
+var rooms_prices = [
+    [r1_name,""],
+    [r2_name,""],
+    [r3_name,""],
+];
+
+$(function() {
     $( "#p1_submit" ).click(function(){
-    	rooms_prices[0][1] = $( "#p1r1_comp" ).val();
-    	rooms_prices[1][1] = $( "#p1r2_comp" ).val();
-    	rooms_prices[2][1] = $( "#p1r3_comp" ).val();
+    	p1_comp[0] = $( "#p1r1_comp" ).val();
+    	p1_comp[1] = $( "#p1r2_comp" ).val();
+    	p1_comp[2] = $( "#p1r3_comp" ).val();
 
-    	$( ".print_comp_r1" ).append( rooms_prices[0][1] );
-        $( ".print_comp_r2" ).append( rooms_prices[1][1] );
-        $( ".print_comp_r3" ).append( rooms_prices[2][1] );
+    	$( ".print_comp_r1" ).append( p1_comp[0] );
+        $( ".print_comp_r2" ).append( p1_comp[1] );
+        $( ".print_comp_r3" ).append( p1_comp[2] );
 
     });
 });
-*/
-
-rooms_prices[0][1] = 250;
-rooms_prices[1][1] = 400;
-rooms_prices[2][1] = 350;
-
-$( ".print_comp_r1" ).append( rooms_prices[0][1] );
-$( ".print_comp_r2" ).append( rooms_prices[1][1] );
-$( ".print_comp_r3" ).append( rooms_prices[2][1] );
 
 $(function() {
     $( "#p2_submit" ).click(function(){
-    
-        p2_fav = $('input[name=p2_fav]:checked').val();
-        var p2_fav_array = rooms_prices[p2_fav];
-        rooms_prices.splice(p2_fav, 1);
-        console.log(rooms_prices);
-        rooms_prices.unshift(p2_fav_array);
+    	p2_fav = $('input[name=p2_fav]:checked').val();
     	p2_runner_up = $('input[name=p2_runner_up]:checked').val();
     	side_pot = $( "#side_pot" ).val();
 
@@ -111,6 +85,12 @@ $(function() {
 });
 
 // dummy filled-in final array
+
+var rooms_prices = [
+    [r1_name, 350],
+    [r2_name, 400],
+    [r3_name, 250],
+    ];
 
 $(function() {
     $( "#p3_submit" ).click(function(){
