0a088c4b3b2206c074ce4d3c7adf163191a27392
marbiru <code@uribram.com>
2015-07-20 13:16:47 +0100

    Revert 80243d9..3177481
    This rolls back to commit 80243d9d5aa44c0b6c23913916f87c4a14681fb4.

--- main.js
+++ main.js
@@ -10,9 +10,8 @@ var r1_name = "";
 var r2_name = "";
 var r3_name = "";
 
-// the main array for storing prices and room names. It gets updated after each player's move. It is an array of three arrays, each of which has the name of a room, the room's current rent/price
+// the main array for storing prices and room names. It gets updated after each player's move. It is an array of three arrays, each of which has the name of a room and then that room's current rent/price 
 
-//almost certainly need "running count" of comp added on the end of each of the arrays here
 var rooms_prices = [
     ["",""],
     ["",""],
@@ -23,7 +22,6 @@ var rooms_prices = [
 var p1_comp = ["", "", ""];
 var p1_gold = "";
 var p1_silver = "";
-var p1_silver_comp = "";
 
 // vars decided by player 2
 var p2_gold = "";
@@ -34,7 +32,7 @@ var side_pot = "";
 var p3_gold = "";
 
 // PLACEHOLDER values!
-total_rent = 1000;
+/*total_rent = 1000;
 
 p1_name = "Lady 1";
 p2_name = "Mr 2";
@@ -54,11 +52,11 @@ $( ".print_r3" ).append( r3_name );
 
 rooms_prices[0][0] = r1_name;
 rooms_prices[1][0] = r2_name;
-rooms_prices[2][0] = r3_name;
+rooms_prices[2][0] = r3_name; */
 
 //END PLACEHOLDER. WHEN DONE, REACTIVATE NEXT FN
 
-/*$(function() {
+$(function() {
     $( "#player_room_submit" ).click(function(){
     	p1_name = $( "#p1_name" ).val();
         p2_name = $( "#p2_name" ).val();
@@ -76,10 +74,10 @@ rooms_prices[2][0] = r3_name;
         $( ".print_r2" ).append( r2_name );
         $( ".print_r3" ).append( r3_name );
 
-        $( "#p1_choices_1" ).show( "slow" );
+        $( "#p1_choices" ).show( "slow" );
         $( "#general_info" ).hide( "slow" );
     });
-}); */
+});
 
 $(function() {
     $( "#p1_continue" ).click(function(){
@@ -103,9 +101,6 @@ $(function() {
         $( ".print_p1_gold_room" ).append( rooms_prices[0][0] );
         $( ".print_p1_silver_room" ).append( rooms_prices[1][0] );
         $( ".print_p1_bronze_room" ).append( rooms_prices[2][0] );
-
-        $( "#p1_choices_2" ).show( "slow" );
-        $( "#p1_choices_1" ).hide( "slow" );
     });
 });
 
@@ -127,22 +122,10 @@ $(function() {
 
 });
 
-$(function() {
-    $( "#p1_continue_2" ).click(function(){
-
-        p1_silver_comp = parseInt($( "#p1_silver_amount" ).val());
-        console.log(p1_silver_comp);
-
-        $( "#p2_choices_3" ).show( "slow" );
-        $( "#p1_choices_2" ).hide( "slow" );
-
-    });
-});
-
 // should re-write this and previous slider function as generalised functions of slider, amount
 
 $(function() {
-
+   
    $( "#p1_bronze_slider" ).slider({
      value: 1,
      min: 1,
@@ -159,9 +142,8 @@ $(function() {
 $(function() {
     $( "#p1_submit" ).click(function(){
 
+        var p1_silver_comp = parseInt($( "#p1_silver_amount" ).val());
         var p1_bronze_comp = parseInt($( "#p1_bronze_amount" ).val());
-
-        p1_comp = [0, p1_silver_comp, p1_bronze_comp];
 
         var tally_p1 = total_rent + p1_silver_comp + p1_bronze_comp;
 
@@ -175,8 +157,8 @@ $(function() {
         $( ".print_p1_silver_comp" ).append( rooms_prices[1][0] + " + $" + p1_silver_comp );
         $( ".print_p1_bronze_comp" ).append( rooms_prices[2][0] + " + $" + p1_bronze_comp );
 
-        $( "#p2_choices_1" ).show( "slow" );
-        $( "#p1_choices_3" ).hide( "slow" );
+        $( "#p2_choices" ).show( "slow" );
+        $( "#p1_choices" ).hide( "slow" );
 
     });
 });
@@ -206,14 +188,12 @@ $(function() {
         $( ".print_p2_silver_room" ).append( rooms_prices[1][0] );
         $( ".print_p2_bronze_room" ).append( rooms_prices[2][0] );
 
-        $( "#p2_choices_2" ).show( "slow" );
-        $( "#p2_choices_1" ).hide( "slow" );
-
     });
 });
 
 $(function() {
    var p2_silver_starting_comp = Number(rooms_prices[1][1]);
+   console.log(p2_silver_starting_comp);
    $( "#p2_silver_slider" ).slider({
      value: p2_silver_starting_comp,
      min: p2_silver_starting_comp,
@@ -239,9 +219,6 @@ $(function() {
 
         side_pot = p2_silver_comp - Number(rooms_prices[1][1]);
 
-        // 1. add sidepot to p2_gold rent
-        // 2. later, subtract sidepot/3 from all rents
-
         console.log(side_pot);
 
         console.log(rooms_prices);
@@ -251,7 +228,7 @@ $(function() {
         $( ".print_p2_bronze_comp" ).append( rooms_prices[2][0] + " at $" + rooms_prices[2][1] + " per month" );
 
         $( "#p3_choices" ).show( "slow" );
-        $( "#p2_choices_2" ).hide( "slow" );
+        $( "#p2_choices" ).hide( "slow" );
 
     });
 });
@@ -267,13 +244,16 @@ $(function() {
 
         //redistributing the side pot -- this needs to go BEFORE splicing, after adjustment has been made in p2_submit to make sure p3 is voting on an appropriate deal by TRIMMING best deal offered p2 (not improving second-best deal)
 
+        console.log(rooms_prices);
+
+
         $( ".print_p3_room" ).append( rooms_prices[0][0] );
         $( ".print_p2_room" ).append( rooms_prices[1][0] );
         $( ".print_p1_room" ).append( rooms_prices[2][0] );
 
-        $( ".print_p3_price" ).append( rooms_prices[0][1].toFixed(2) );
-        $( ".print_p2_price" ).append( rooms_prices[1][1].toFixed(2) );
-        $( ".print_p1_price" ).append( rooms_prices[2][1].toFixed(2) );
+        $( ".print_p3_price" ).append( rooms_prices[0][1] );
+        $( ".print_p2_price" ).append( rooms_prices[1][1] );
+        $( ".print_p1_price" ).append( rooms_prices[2][1] );
 
         $( "#results" ).show( "slow" );
         $( "#p3_choices" ).hide( "slow" );
